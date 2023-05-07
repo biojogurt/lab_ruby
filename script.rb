7ac45fa4982d2c6f1bbaa8d6dc7c7eac31d18a07
biojogurt <anathebeequeen@gmail.com>
2023-05-07 17:58:44 +0300

    Initial commit

---
+++ script.rb
@@ -0,0 +1,11 @@
+string = "The quick brown fox jumps over the lazy dog"
+substring = "quick brown "
+
+puts("Строка: " + string)
+puts("Подстрока: " + substring)
+
+substringIndex = string.index(substring)
+result = string.sub(substring, "")
+
+puts("Начало удаленной подстроки: " + substringIndex.to_s)
+puts("Новая строка: " + result)
